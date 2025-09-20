3979919d4d188a6c7d23a98e6f192ef090966d87
Rohit Mehta <rohitkumarmehta25@gmail.com>
2025-09-21 03:16:23 +0530

    added touch for mobile

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,7 +1,9 @@
+"use client";
 import GithubIcon2 from "@/assets/icons/GithubIcon2";
 import { cn } from "@/lib/utils";
 import { ArrowUpRight } from "lucide-react";
 import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
 
 interface ProjectCardProps {
   image: StaticImageData;
@@ -22,8 +24,13 @@ export default function ProjectCard({
   repo,
   className,
 }: ProjectCardProps) {
+  const [touched, setTouched] = useState(false);
   return (
-    <div className={cn("p-4 group h-auto", className)}>
+    <div
+      onTouchStart={() => setTouched(true)}
+      onTouchEnd={() => setTouched(false)}
+      className={cn("p-4 group h-auto", className)}
+    >
       <div className="border bg-secondary flex flex-col justify-between gap-4 h-full w-full shadow-md">
         <div className="flex flex-col gap-4 px-4 pt-4">
           <a href={link} target="_blank" rel="noopener noreferrer">
@@ -35,7 +42,12 @@ export default function ProjectCard({
                 loading="lazy"
                 className="object-cover rounded-xl group-hover:scale-105 transition-all duration-300 group-hover:blur-[1px]"
               />
-              <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 active:opacity-100">
+              <div
+                className={cn(
+                  "absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300",
+                  touched ? "opacity-100" : "opacity-0"
+                )}
+              >
                 <h2 className="text-3xl md:text-5xl font-semibold text-white transform translate-y-5 group-hover:translate-y-0 transition-all duration-300">
                   {title}
                 </h2>
